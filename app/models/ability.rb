bc2eecdf73a72a98524825e1f3761870b98c81f9
TuBaDuc <tubaduc@gmail.com>
2017-02-07 11:44:13 +0700

    2. login logout step4: add role

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize user
+    user ||= User.new
+    case user.role
+    when :admin
+      can :manage, :all
+    when :user
+      can :read, User
+      can :edit, User do |check_user|
+        check_user == user
+      end
+    else
+      can :read, User
+    end
+  end
+end
